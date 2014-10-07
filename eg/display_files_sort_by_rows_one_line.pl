ca923366d6c7f2ede36689f6edd7b3659fa202c0
Nikolay Mishin <mi@ya.ru>
2014-10-08 00:49:58 +0400

    Update display_files_sort_by_rows_one_line.pl

--- eg/display_files_sort_by_rows_one_line.pl
+++ eg/display_files_sort_by_rows_one_line.pl
@@ -1,2 +1,4 @@
 #Display a list files with mask /regex/ in the current directory, sorted by the number of rows
 perl -MIO::All -E "$p{$_->name}=map{$l++}$_->slurp for grep{/regex/}io('.')->all;for$k(sort{$p{$a}<=>$p{$b}}keys%p){say qq{$k $p{$k}}}"
+#better solutions
+perl -MIO::All -E'$p{$_->name}=$_->getlines for grep{/perlre/}io->dir(".")->all;say"$_ $p{$_}"for sort{$p{$a}<=>$p{$b}}keys%p'
